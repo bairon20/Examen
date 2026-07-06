a4a3cbc7d78f3bff850e6cd79ba81d4a19e4998f
bairon20 <br.gomezv@duocuc.cl>
2026-07-06 11:57:21 -0400

    Initial commit of dockerized monorepo with ECR workflows

---
+++ front_despacho/src/config.js
@@ -0,0 +1,2 @@
+export const API_VENTAS_URL = import.meta.env.VITE_API_VENTAS_URL || "http://localhost:8080/api/v1/ventas";
+export const API_DESPACHOS_URL = import.meta.env.VITE_API_DESPACHOS_URL || "http://localhost:8081/api/v1/despachos";
